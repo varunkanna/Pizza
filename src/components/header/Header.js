4e547c9beac71c12de30b9253a14a1786330fe0d
Gangeswaran K <96485946+varunkanna@users.noreply.github.com>
2023-12-01 07:59:18 +0530

    header added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,9 +7,18 @@ const Header = () => {
     <div className='header'>
         <div className='left'>
           <img src={imageBag.logo} className='pizza-logo' alt='logo' />
+          <a href='#' className='Head-text'>Cool!
+          <li className='colored-text'>Pizza</li>
+          </a>
         </div>
-        <div className='center'></div>
-        <div className='right'></div>
+        <div className='center'>
+          <li><a>Buy Pizza</a></li>
+          <li>Contact</li>
+          <li>Menu</li>
+        </div>
+        <div className='right'>
+          <button className='sms-btn'>Send message</button>
+        </div>
     </div>
   )
 }
